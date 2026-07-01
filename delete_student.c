87fdfa9724f8f6d5d076545344c209d13551a70d
Penukonda Lohithanand <lohith.anand.penukonda@gmail.com>
2026-07-01 13:16:44 +0530

    Add delete_student.c to delete a student by ID

---
+++ delete_student.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int studentID;
+
+    printf("Enter Student ID to Delete: ");
+
+    scanf("%d", &studentID);
+
+    printf("Student with ID %d deleted successfully.", studentID);
+
+    return 0;
+}
